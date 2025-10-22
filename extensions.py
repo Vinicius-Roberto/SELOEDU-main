7671f52fa74b82eaec0f87b8fc7078000a17950b
vinícius <viniciusrobertogd@gmail.com>
2025-10-22 16:13:16 -0300

    correção de importação

--- extensions.py
+++ extensions.py
@@ -1,14 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_mail import Mail  # Corrigido: deve importar de flask_mail
+from flask_mail import Mail  
 
-# Inicialização do banco de dados
 db = SQLAlchemy()
 
-# Configuração do gerenciador de login
 login_manager = LoginManager()
-login_manager.login_view = "auth.login"  # rota para redirecionar usuários não logados
+login_manager.login_view = "auth.login" 
 login_manager.login_message = "Por favor, faça login para acessar esta página."
 
-# Inicialização do e-mail
 mail = Mail()
